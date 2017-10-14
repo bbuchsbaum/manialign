866b3123d30e575a0066f7e372a69732f9474069
Bradley Buchsbaum <bbuchsbaum@Fantail.local>
2017-10-13 20:12:15 -0400

    lpp

--- R/lpp.R
+++ R/lpp.R
@@ -2,17 +2,28 @@
 #'
 #' @param X the data matrix
 #' @param W the affinity matrix
-#' @param ndim number of dimensions
+#' @param ncomp number of latent components
 #' @importFrom RSpectra eigs
 #' @export
-embed_lpp <- function(X, W, ndim=2) {
+lpp <- function(X, W, ncomp=2, reduced_rank=ncol(X), center=TRUE, scale=FALSE) {
+  assert_that(nrow(X) == nrow(W))
 
-  D <- Diagonal(x=rowSums(W))
-  L <- D - W
+  X <- neuroca::pre_processor(X)
 
-  Zl <- t(X) %*% L %*% X
-  Zr <- t(X) %*% D %*% X
+  d <- rowSums(W)
+  D <- Diagonal(x=d)
+  D_mhalf <- Diagonal(x=d^(-1/2))
 
-  decomp <- RSpectra::eigs(solve(Zr,Zl), k=ndim, which="SM")
+  W <- D_mhalf %*% W %*% D_mhalf
+  #W <- neighborweights:::psparse(W, max)
+
+  Xp <- D^.5 %*% X
+
+  #L <- D - W
+  #Zl <- t(X) %*% L %*% X
+  #Zr <- t(X) %*% D %*% X
+  #decomp <- RSpectra::eigs(solve(Zr,Zl), k=ndim, which="SM")
+
+  res <- lge(Xp, W, D=NULL, ncomp=ncomp, reduced_rank=reduced_rank)
 
 }
